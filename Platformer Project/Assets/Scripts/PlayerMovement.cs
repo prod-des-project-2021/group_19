fb8574e4fa0a16371e62e1f6514ead427b906138
Anssi Hautamäki <58326078+Anhautam@users.noreply.github.com>
2021-11-16 22:58:35 +0200

    Basic movement script created

---
+++ Platformer Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]private float speed;
+    private Rigidbody2D body;
+    private bool grounded;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if(Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+}
